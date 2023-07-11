e82b32bd628ba2740eec69b3d258d923497efa4f
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-07-11 13:22:25 +0600

    product design added

--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -4,9 +4,9 @@ const Product = ({ product }) => {
     console.log(product);
     const {title, image, price, ratting} = product
     return (
-        <div className=''>
-            <img className='h-[100px]' src={image} alt="" />
-            <div>
+        <div className='bg-gray-100 py-6 px-2'>
+            <img className='h-[100px] justify-center mx-auto' src={image} alt="" />
+            <div className='text-center'>
                 <h2>{title}</h2>
                 <p>{price}</p>
                 <p><small>{ratting?.rate}</small></p>
--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -4,15 +4,25 @@ import Product from "./Product";
 const Products = () => {
     const [products, setProducts] = useProducts();
     return (
-        <div className="container mx-auto">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
-                {
-                    products.map(product => <Product key={product._id}
-                        product={product}
-                    ></Product>)
-                }
-            </div>
+        <div className="bg-gray-200 py-4">
+            <div className="container mx-auto">
+                <div className="bg-white mt-8 p-6">
+                    <div className="flex items-center justify-between">
+                        <h2 className="text-3xl mb-2">Top Popular products</h2>
+                        <button className="bg-orange-500 text-white px-4 py-1 rounded-md">see more</button>
+                    </div>
+                    <hr />
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 pt-5 ">
 
+                        {
+                            products.slice(0,15).map(product => <Product key={product._id}
+                                product={product}
+                            ></Product>)
+                        }
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
